843c858e950bd89f71ebef4f93a379f90793f31e
Daniel Lüke <daniel.lueke@t-online.de>
2019-06-29 13:07:42 +0200

    Adjusted environment.yml and travis_runner in an attempt to fix travis

--- utils/travis_runner.py
+++ utils/travis_runner.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python
 """This script manages all tasks for the TRAVIS build server."""
 import subprocess as sp
-import glob
 
 if __name__ == '__main__':
 
-    for notebook in glob.glob('*.ipynb'):
-        cmd = ' jupyter nbconvert --execute {}  --ExecutePreprocessor.timeout=-1'.format(notebook)
+    notebook = 'student_project.ipynb'
+    cmd = ' jupyter nbconvert --execute {}  --ExecutePreprocessor.timeout=-1'.format(notebook)
 sp.check_call(cmd, shell=True)
